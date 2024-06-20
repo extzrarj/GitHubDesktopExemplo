8b6cd76b5c74ab1c2fedededac0a7ce76ac598a0
Ester Araujo <estarj175@gmail.com>
2024-06-19 21:55:46 -0300

    Criação do projeto

---
+++ GitHubDesktopExemplo/src/main/java/com/mycompany/githubdesktopexemplo/GitHubDesktopExemplo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.githubdesktopexemplo;
+
+/**
+ *
+ * @author User
+ */
+public class GitHubDesktopExemplo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
